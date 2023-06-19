8287e4d46d91283aa2dd0697528d1ce2bb84c4f2
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-06-19 15:42:15 +0300

    finished array filter method lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+const findMatching = function(drivers, name) {
+    let match = drivers.filter((item) => item.toLowerCase() === name.toLowerCase())
+    return match
+}
+
+const fuzzyMatch = function(drivers, name) {
+    let match
+    match = drivers.filter((item)=> item.charAt(0) === name.charAt(0))
+    return match
+}
+
+const matchName = function(drivers, name) {
+    let match
+    match = drivers.filter((item) => item.name === name)
+    return match
+}
